f74f721559e418009a660efbc22c553457d61fd8
rainmaker514 <tdoug016@fiu.edu>
2024-04-12 16:24:18 -0400

    minor changes to about us section

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const navbarMenu = document.getElementById("navbar-menu");
 const overlayHero = document.getElementById("overlay-hero");
 const overlay = document.getElementById("overlay");
 const mobileMenuIcon = document.getElementById("mobile-menu-icon");
-// const text = document.getElementById("hero-text");
-const elementsWithText = document.querySelectorAll('[class*="text"]');
+const heroText = document.getElementById("hero-text");
 const button = document.querySelector(".hero-button");
 
 //DISPLAYS OVERLAY WHEN ICON IS CLICKED
@@ -14,21 +13,13 @@ mobileMenuIcon.addEventListener("click", () => {
   overlay.classList.toggle("active");
   navbarMenu.classList.toggle("active");
   if (overlayHero.classList.contains("active")) {
-    // text.style.color = "rgba(255, 255, 255, 0.1)";
-    elementsWithText.forEach((element) => {
-      element.style.color = "rgba(255, 255, 255, 0.1)";
-    });
+    heroText.style.color = "rgba(255, 255, 255, 0.1)";
     button.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
   } else {
-    // text.style.color = "white";
-    elementsWithText.forEach((element) => {
-      element.style.color = "white";
-    });
+    heroText.style.color = "white";
     button.style.backgroundColor = "goldenrod";
   }
 });
-
-// have an event listener that listens for the overlay becoming active, then loop through each element looking for whatever contains "text" and dim the color
 
 //COLLAPSES NAVBAR-MENU AND REMOVES OVERLAY WHEN IN MOBILE VIEW AND MOBILE-MENU-TOGGLE CHECKBOX IS CHECKED
 navbarLinks.forEach((navbarLink) => {
@@ -44,10 +35,7 @@ function toggleNavbarMenu() {
   navbarMenu.classList.toggle("active");
   overlayHero.classList.toggle("active");
   overlay.classList.toggle("active");
-  // text.style.color = "white";
-  elementsWithText.forEach((element) => {
-    element.style.color = "white";
-  });
+  heroText.style.color = "white";
   button.style.backgroundColor = "goldenrod";
   mobileMenuToggle.checked = false;
 }
